65ff759e9a27f00909e4c01c83f6612d8f64f6cc
sborrillo <sborrillo@gmail.com>
2022-03-22 23:28:20 +0100

    La decima es la vencida?

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
